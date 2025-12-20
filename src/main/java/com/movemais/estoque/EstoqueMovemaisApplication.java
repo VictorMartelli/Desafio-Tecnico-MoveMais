e32450083b0b02291ab89169fbb64a784fb176ea
VictorMartelli <victormartelligp@gmail.com>
2025-12-20 08:36:10 -0300

    chore: initial project structure with Spring Boot 3.5.9 and Java 25

---
+++ src/main/java/com/movemais/estoque/EstoqueMovemaisApplication.java
@@ -0,0 +1,13 @@
+package com.movemais.estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueMovemaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueMovemaisApplication.class, args);
+	}
+
+}
